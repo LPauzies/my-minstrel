1aca4c39d6d583946cb8f459438c5a17e6239392
Lucas Pauzies <lucas.pauzies@capgemini.com>
2022-08-01 16:55:20 +0200

    WIP Multi filter

--- src/app/domains/transformations/transformer.ts
+++ src/app/domains/transformations/transformer.ts
@@ -9,7 +9,11 @@ export class Transformer {
         return json.videos.map((e: any) => new YoutubeVideo(e.id, e.title, e.keywords));
     }
 
-    public static fromJSONtoFilter(json: any, type: string): Array<FilterVideo> {
+    public static fromJSONtoFilterMacro(json: any): Array<FilterVideo> {
+        return Object.keys(json['filters']).map((e: string) => new FilterVideo(e));
+    }
+
+    public static fromJSONtoFilterMicro(json: any, type: string): Array<FilterVideo> {
         return json['filters'][type].map((e: string) => new FilterVideo(e));
     }
 
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -10,33 +10,24 @@ import { YoutubeVideo } from '../domains/youtubeVideo';
 })
 export class DataService {
 
-    static DATA: Array<YoutubeVideo> = []
-    static FILTERS: Array<FilterVideo> = []
+    static DATA: Array<YoutubeVideo> = [];
+    static FILTERS_MACRO: Array<FilterVideo> = [];
+    static FILTERS_MICRO: Array<FilterVideo> = [];
 
     private dataSubject = new BehaviorSubject<Array<YoutubeVideo>>(DataService.DATA);
     private currentData = this.dataSubject.asObservable();
-    private filterMusicSubject = new BehaviorSubject<Array<FilterVideo>>(DataService.FILTERS);
-    private currentMusicFilters = this.filterMusicSubject.asObservable();
-    private filterAmbientSubject = new BehaviorSubject<Array<FilterVideo>>(DataService.FILTERS);
-    private currentAmbientFilters = this.filterAmbientSubject.asObservable();
 
     constructor(
         private httpClient: HttpClient
     ) {
         this.httpClient.get(`./assets/data/data.json`).subscribe(json => this.dataSubject.next(Transformer.fromJSONtoYoutubeVideos(json)));
-        this.httpClient.get(`./assets/data/filters.json`).subscribe(json => this.filterMusicSubject.next(Transformer.fromJSONtoFilter(json, "music")));
-        this.httpClient.get(`./assets/data/filters.json`).subscribe(json => this.filterAmbientSubject.next(Transformer.fromJSONtoFilter(json, "ambient")));
     }
 
     getData(): Observable<Array<YoutubeVideo>> {
         return this.currentData;
     }
 
-    getMusicFilters(): Observable<Array<FilterVideo>> {
-        return this.filterMusicSubject;
-    }
-
-    getAmbientFilters(): Observable<Array<FilterVideo>> {
-        return this.filterMusicSubject;
+    getFilters(): Observable<any> {
+        return this.httpClient.get(`./assets/data/filters.json`);
     }
 }
